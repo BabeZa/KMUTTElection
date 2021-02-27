afabafc4ff2abfbf527fee2650e27910b5720c57
BabeZa <babesevenseven@gmail.com>
2021-02-27 14:18:48 +0700

    Updates

---
+++ precache-manifest.3bc1e6ab6d18d077ec69c2d77b411665.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7dea23c852164d3f72a7cc29f7f33f",
+    "url": "/KMUTTElection/index.html"
+  },
+  {
+    "revision": "cf377efe0c136986cde4",
+    "url": "/KMUTTElection/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7266619e30aec4d2ecf9",
+    "url": "/KMUTTElection/static/css/main.8ea325f2.chunk.css"
+  },
+  {
+    "revision": "cf377efe0c136986cde4",
+    "url": "/KMUTTElection/static/js/2.6c5c6414.chunk.js"
+  },
+  {
+    "revision": "21f38193c3caf91fe32157540d33cdde",
+    "url": "/KMUTTElection/static/js/2.6c5c6414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7266619e30aec4d2ecf9",
+    "url": "/KMUTTElection/static/js/main.3ceebc5d.chunk.js"
+  },
+  {
+    "revision": "20bb1de667c08753d3ca",
+    "url": "/KMUTTElection/static/js/runtime-main.36988b7d.js"
+  },
+  {
+    "revision": "2efcb640a45f2b3c512545104a14b4aa",
+    "url": "/KMUTTElection/static/media/Avatar.2efcb640.png"
+  },
+  {
+    "revision": "663db208643e98c28ffa7bccb87645b1",
+    "url": "/KMUTTElection/static/media/KMUTT.663db208.png"
+  },
+  {
+    "revision": "4fb40fc9f5ec1e0a36f6e1faa90e714c",
+    "url": "/KMUTTElection/static/media/check.4fb40fc9.png"
+  },
+  {
+    "revision": "7b307b382cd63fb0b159e146cb5be7ee",
+    "url": "/KMUTTElection/static/media/group.7b307b38.jpg"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/KMUTTElection/static/media/instagram.097b7749.png"
+  }
+]);
